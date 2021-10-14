44a3cd150fa3c6d4a08e81ba40b025533dd6d998
Kiharson <kiharabhi@gmail.com>
2021-10-14 12:21:53 +0530

    Initial commit

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gameController : MonoBehaviour
+{
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void changeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Measuring Tape
+ ARCore based Android application built using Unity Game Engine and Google’s ARCore SDK. The Android application uses features of ARCore such as Motion Tracking, Light Estimation and Environment Understanding to measure dimension of objects and distance between objects
